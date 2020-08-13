c7b043d645b3f8dba2fbf84ff6a3822d10e48ef6
peterhchen <peter.hp.chen@gmail.com>
2020-08-12 18:07:15 -0700

    Add files via upload

---
+++ sec02_030-polish-form/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,27 @@
+
+// import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Post } from '../post.model';
+import { PostsService } from './../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+  enterContent = 'NO CONTEXT';
+  enterTitle = 'No TITLE';
+  // newPost = 'NO CONTEXT';
+  // @Output() postCreated = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) { }
+  ngOnInit() { }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
+  }
+}
